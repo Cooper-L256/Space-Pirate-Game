d24f244f824fbc9579482f3de94fdb10a4e57aa0
kaleb-ries <kalebries09@gmail.com>
2026-04-19 22:50:02 -0700

    Fix issue that commented out the Ship class definition in Ship.java and hopefully fix bad naming convension that confused java in Main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@
 
 
 // This is a begaining page when a user launches the program they will land on this page 1st like welcoming message (Theo)
-public class main {
+public class Main {
    public static void main(String[] args) {
       System.out.println("Hello Ship");
       System.out.println(" Is");
