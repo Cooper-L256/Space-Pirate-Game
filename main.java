018a393f21186d69db77eb7fd90bbeb85badb3c8
Cooper Lauer <c2g-cooper@outlook,com>
2026-04-20 17:27:47 -0700

    Updated Ship.java. creating a secondary constructor as well as creating getter and setter methods for the hp variable. Created the damageRange variable, an int array to represent the range of a ship's attack power. Created respective getters and setters for damageRange. Created the takeDamage() method, which decreases the Ship's hp. Created the attack() method, which uses takeDamage() to deal a random number of damage within range to another Ship object.

--- main.java
+++ main.java
@@ -7,6 +7,6 @@ public class Main {
       System.out.println("Hello Ship");
       // Create a ship
       Ship ship = new Ship(10);
-      System.out.println("Ship is currently at coordinates ("+ship.x+", "+ship.y+")");
+   System.out.println("Ship is currently at coordinates ("+ship.x+", "+ship.y+")");
    }
 }
