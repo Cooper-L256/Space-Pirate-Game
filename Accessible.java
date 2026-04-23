b6fda080e2c02b5a0370c236b45dcfa4a7ce9208
kaleb-ries <kalebries09@gmail.com>
2026-04-22 23:35:00 -0700

    Edit Accessible.java to keep arrow key navigation from wrapping to the top/bottom of the elements list when one end of the list was reached

--- Accessible.java
+++ Accessible.java
@@ -1,3 +1,7 @@
+// TODO
+// Set up invisible JLabel and make methods for using accessibleContext to announce to screen readers
+// This is the best way to manually send text to screen readers alone
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,10 +11,12 @@ import java.awt.event.*;
 
 public class Accessible {
 	public static void modifyContainer(Container c) {
+		c.setFocusCycleRoot(true);
 		JComponent jcomp = (JComponent) c; // Converts the container to a more generic class that has important functionality for the purposes of this method
-		// The following two lines make it so that any widgets inside the modified container can be navigated with the up and down arrow keys via a screenreader
-		bind(jcomp, KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up", new RunnableAction(() -> KeyboardFocusManager.getCurrentKeyboardFocusManager().focusPreviousComponent()));
-		bind(jcomp, KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "down", new RunnableAction(() -> KeyboardFocusManager.getCurrentKeyboardFocusManager().focusNextComponent()));
+		// The following lines make it so that any widgets inside the modified container can be navigated with the up and down arrow keys via a screenreader
+		
+		bind(jcomp, KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up", new MoveFocus(0)); // Move to previous element on up arrow key
+		bind(jcomp, KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "down", new MoveFocus(1)); // Move to next element on down arrow key
 		// The following code tracks whenever a widget is added to the modified container and makes it focusable / navigable to screen readers, which mostly just affects JLabels (plain text widgets) which are not focusable by default
 		c.addContainerListener(new ContainerAdapter() {
 			@Override
@@ -21,19 +27,39 @@ public class Accessible {
 		});
 	}
 	// A method that makes adding keyboard event listeners easier
-	public static void bind(JComponent c, KeyStroke keyStroke, String actionName, RunnableAction action) {
+	public static void bind(JComponent c, KeyStroke keyStroke, String actionName, MoveFocus action) {
 		c.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keyStroke, actionName);
 		c.getActionMap().put(actionName, action);
 	}
 	// I seem to have left out the method that unbinds a given binding, but for now it's not needed and may never be anyway
 }
 // The following is an event class used above that allows any given function/method to be used as the argument/action to be performed when the event triggers
-class RunnableAction extends AbstractAction {
-	private Runnable action; // A special java object for containing functions
-	public RunnableAction(Runnable a) {
-		action = a;
+class MoveFocus extends AbstractAction {
+	int dir;
+	public MoveFocus(int dir) {
+		this.dir = dir;
 	}
 	public void actionPerformed(ActionEvent evt) {
-		action.run(); // Runs the function when the event fires
+		KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+		Component focus = manager.getFocusOwner();
+		if (focus != null) {
+			Container parent = focus.getParent();
+			FocusTraversalPolicy policy = parent.getFocusTraversalPolicy();
+			if (dir == 0) {
+				if (focus.equals(policy.getFirstComponent(parent))) {
+					// Play sound later
+					Toolkit.getDefaultToolkit().beep();
+				} else {
+					manager.focusPreviousComponent();
+				}
+			} else if (dir == 1) {
+				if (focus.equals(policy.getLastComponent(parent))) {
+					// Play sound later
+			Toolkit.getDefaultToolkit().beep();
+				} else {
+				manager.focusNextComponent();
+				}
+			}
+		}
 }
 }
